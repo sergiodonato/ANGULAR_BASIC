4fab439bbb89f3ab41952f3d3837d2e51d80f89f
Sergio Donato <sergiodcdn@gmail.com>
2023-07-26 18:48:25 -0300

    directives style+class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ParentDateComponent } from './components/parent-date/parent-date.component';
+import { DirectivesComponent } from './components/directives/directives.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponentComponent,
-    ParentDateComponent
+    ParentDateComponent,
+    DirectivesComponent
   ],
   imports: [
     BrowserModule,
