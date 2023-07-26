4089a6c317d5a06ba60fd43af3e439cdc2c7c43d
Sergio Donato <sergiodcdn@gmail.com>
2023-07-26 17:39:26 -0300

    dados do fist-component, exemplos deles no html

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
-  styleUrls: ['./first-component.component.css']
+  styleUrls: ['./first-component.component.css'],
 })
 export class FirstComponentComponent {
-
+  name: string = 'Teteco';
+  age: number = 4;
+  job = 'jardineiro mirim';
+  hobbies = ['pular', 'morder', 'morder']
+  cor = {
+    main: 'amarelo',
+    secondary: 'creme'
+  }
 }
