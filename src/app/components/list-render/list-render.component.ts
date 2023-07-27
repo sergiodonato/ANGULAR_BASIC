cc09bff24b7299ca03e6f9b33ea3c08c02c092f7
Sergio Donato <sergiodcdn@gmail.com>
2023-07-27 14:59:10 -0300

    interfaces -> Animal.ts

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,15 +1,33 @@
 import { Component } from '@angular/core';
 
+import { Animal } from 'src/app/Animal';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
-  styleUrls: ['./list-render.component.css']
+  styleUrls: ['./list-render.component.css'],
 })
 export class ListRenderComponent {
-  animals = [
-    {name: 'Minie', type: 'Dog'},
-    {name: 'Kai', type: 'Cat'},
-    {name: 'To', type: 'Cat'},
-    {name: 'Te', type: 'Cat'}
-  ]
+  animals: Animal[] = [
+    { name: 'Minie', type: 'Dog', age: 12 },
+    { name: 'Kai', type: 'Cat', age: 6 },
+    { name: 'To', type: 'Cat', age: 6 },
+    { name: 'Te', type: 'Cat', age: 4 },
+  ];
+
+  animal: Animal = {
+    name: 'Sergio',
+    type: 'Human',
+    age: 37,
+  };
+
+  animalDetails = '';
+
+  showAge(animal: Animal): void {
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos!`;
+  }
+
+  showType(animal: Animal) {
+    this.animalDetails += `O pet ${animal.name} é um ${animal.type}!`;
+  }
 }
