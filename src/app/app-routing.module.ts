e30042fd243f65cfd88921a35cf412a2593089c5
Sergio Donato <sergiodcdn@gmail.com>
2023-07-28 10:43:05 -0300

    implementação de Angular Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { ChangeNumberComponent } from './components/change-number/change-number.component';
+import { PipesComponent } from './components/pipes/pipes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FirstComponentComponent },
+  { path: 'list', component: ListRenderComponent },
+  { path: 'pipe', component: PipesComponent },
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
